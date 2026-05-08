30bd0ba87052eafbbbb1300636e764090c868be3
Nicolas Dupuy <nicolas0dupuy@gmail.com>
2026-05-08 10:48:19 +0000

    adding_content

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+const roles = ["researcher", "data scientist"];
+let roleIndex = 0;
+const roleElement = document.getElementById("role-loop");
+
+if (roleElement) {
+    setInterval(() => {
+        roleIndex = (roleIndex + 1) % roles.length;
+        roleElement.textContent = roles[roleIndex];
+    }, 2500);
+}
